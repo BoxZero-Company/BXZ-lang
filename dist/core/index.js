29d44d553bf8d665bd4d79f330480de77cf70ef3
BoxZero™ <samyarsol92@gmail.com>
2026-09-18 14:42:33 +0330

    Delete dist directory

--- dist/core/index.js
+++
@@ -1,8 +0,0 @@
-// src/cli/core/coreFunc1.js
-function logMessage(message) {
-  console.log(`[Core Function 1]: ${message}`);
-}
-
-// src/cli/core/index.js
-console.log("Running build:core...");
-logMessage("Core initialization complete.");
